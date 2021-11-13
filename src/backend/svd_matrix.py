DzakyFattan/EigenVector-SimpleImageCompressor

commit f4dee5d42c3db6a87ead5cae01f5c07be539d1a3

Prioritise matrix inversion over transposition when calculating left_singular_inv

src/backend/svd_matrix.py
@@ -130,11 +130,16 @@ def ortho_singular_right(matrix, left_singular, diagonal_singular):
         if diagonal_singular[i][i] < 1e-16:
             break
 
-    diag_singular = np.zeros((np.shape(matrix)[1], np.shape(matrix)[0]))
+    diag_singular_inv = np.zeros((np.shape(matrix)[1], np.shape(matrix)[0]))
     inv_diag = np.linalg.inv(diagonal_singular[:i, :i])
-    diag_singular[:i, :i] = inv_diag
+    diag_singular_inv[:i, :i] = inv_diag
 
-    right_singular = diag_singular @ np.transpose(left_singular) @ matrix
+    try:
+        left_singular_inv = np.linalg.inv(left_singular)
+    except np.linalg.LinAlgError:
+        left_singular_inv = np.transpose(left_singular)
+
+    right_singular = diag_singular_inv @ left_singular_inv @ matrix
 
     return right_singular
 

commit da4528326e8c0c23a3833257399d2db3a11bd6d4

Choose transposition when calculating inverse of left singular matrix
Regression from f4dee5d42c3db6a87ead5cae01f5c07be539d1a3

src/backend/svd_matrix.py
@@ -134,10 +134,7 @@ def ortho_singular_right(matrix, left_singular, diagonal_singular):
     inv_diag = np.linalg.inv(diagonal_singular[:i, :i])
     diag_singular_inv[:i, :i] = inv_diag
 
-    try:
-        left_singular_inv = np.linalg.inv(left_singular)
-    except np.linalg.LinAlgError:
-        left_singular_inv = np.transpose(left_singular)
+    left_singular_inv = np.transpose(left_singular)
 
     right_singular = diag_singular_inv @ left_singular_inv @ matrix
 